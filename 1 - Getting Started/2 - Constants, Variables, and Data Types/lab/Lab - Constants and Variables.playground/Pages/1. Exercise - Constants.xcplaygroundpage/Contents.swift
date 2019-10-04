08070617167abcf392fd5fc8db51140e5f824159
Jason Liu <jliu@lcs.on.ca>
2019-10-03 20:51:55 -0400

    Done 1.1.2

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -13,6 +13,6 @@ friends = 666
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
  */
-
+print("var should be used rather than let when people want to change the value of a constant")
 
 //: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
