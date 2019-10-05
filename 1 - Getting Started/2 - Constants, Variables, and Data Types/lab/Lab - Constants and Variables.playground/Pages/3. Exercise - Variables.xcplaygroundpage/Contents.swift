a94c001b4e8e0815f77b048a8ce0468e96469f56
Jason Liu <jliu@lcs.on.ca>
2019-10-05 14:24:10 -0400

    Finished 1.1.3

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -3,16 +3,17 @@
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
-
+var scholling = 11
 
 /*:
  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
  */
-
+scholling += 1
+print("Years of school that I have completed\(scholling)")
 
 /*:
  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
  */
-
+print("var allows changes but let does not allow for any changes")
 
 //: [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
